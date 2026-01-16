da28472aba3a4a9c8ed11d00779e0e326080a1e8
gtjes <juanjesus13807@gmail.com>
2026-01-16 11:46:51 -0600

    entrega 1

--- src/main.c
+++ src/main.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
 
-int main(int argc, char *argv)
+
+float area (float side)
 {
-    printf("Hello world!\n");
-    return 0;
+   float a = side * side;
+   return a;
+}
+
+float volume (float side)
+{
+
+   float v = side * side * side;
+   return v;
+
+}
+int main()
+{
+   float side = 0;
+
+   if(scanf("%f", &side) != 1)
+   {
+      printf(" Error the side must be a number\n");
+
+      return 1;
+   }
+   if( side < 0 )
+   {
+      printf("Errror the number cannot be negative\n");
+
+      return 1;
+   }
+
+   printf (" Ententer the side length:");
+
+   scanf("%f", &side);
+   
+   float a = area(side);
+   float v = volume(side);
+
+   printf("square area: %f, volume: %f\n", a, v);
+
+   return 0;
 }
